3ca8ebc325ae4df75bad3446785687967f68197b
Bulbizar <adrienesteves@icloud.com>
2023-05-10 11:49:28 +0200

    add disk info, cpu info and memory info

--- Agents/cpu.py
+++ Agents/cpu.py
@@ -1,3 +1,4 @@
+import datetime as datetime
 import psutil
 import datetime
 import time
@@ -8,13 +9,35 @@ logging.basicConfig(level=logging.INFO,
                     filemode="a",
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-try:    
+count = 1
+
+#CPU INFOS
+try:
       while True:
-            print(datetime.datetime.now().time(),
-            psutil.cpu_times_percent(interval=0, percpu=True),
-            )
-            logging.info(psutil.cpu_times_percent(interval=0))
-            time.sleep(10)
+          match count:
+              case 1:
+                  print(datetime.datetime.now().time(),
+                        psutil.cpu_times_percent(interval=0, percpu=True),
+                        )
+                  logging.info(psutil.cpu_times_percent(interval=0))
+                  count += 1
+                  time.sleep(10)
+              case 2:
+                  print(datetime.datetime.now().time(),
+                        psutil.virtual_memory(),
+                        )
+                  logging.info(psutil.virtual_memory())
+                  count += 1
+                  time.sleep(10)
+              case 3:
+                  print(datetime.datetime.now().time(),
+                        psutil.disk_io_counters(perdisk=False, nowrap=True)
+                        )
+                  logging.info(psutil.disk_io_counters(perdisk=False, nowrap=True))
+                  count += 1
+                  time.sleep(10)
+
+
 except Exception as e:
       print (str(e).capitalize())
 except KeyboardInterrupt:
