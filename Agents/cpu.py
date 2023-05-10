3f23119b58d239fbeddf0d3997b55a5e9465a068
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2023-05-10 11:12:08 +0200

    feat/agent adding the cpu stories

--- Agents/cpu.py
+++ Agents/cpu.py
@@ -3,21 +3,20 @@ import datetime
 import time
 import logging
 
-temps = 1
-max_temps = 4
-
-count = 1
-
-while count < max_temps:
-    print(datetime.datetime.now().time(),
-          psutil.cpu_times_percent(interval=1))
-    time.sleep(temps)
-    count += 1
-
 logging.basicConfig(level=logging.DEBUG,
                     filename="log.log",
                     filemode="a",
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-logging.info(psutil.cpu_times_percent(interval=1))
+try:    
+      while True:
+            print(datetime.datetime.now().time(),
+            psutil.cpu_times_percent(interval=0, percpu=True),
+            )
+            logging.info(psutil.cpu_times_percent(interval=0))
+            time.sleep(10)
+except Exception as e:
+      print (str(e).capitalize())
+except KeyboardInterrupt:
+      print ("Finished")
