43755bf89360bf7404852a19d3028beaf9319da1
Bulbizar <adrienesteves@icloud.com>
2023-05-10 11:04:09 +0200

    get cpu info, virtual memory info

--- Agents/cpu.py
+++ Agents/cpu.py
@@ -4,20 +4,20 @@ import time
 import logging
 
 temps = 1
-max_temps = 4
+max_temps = 1
 
 count = 1
 
-while count < max_temps:
-    print(datetime.datetime.now().time(),
-          psutil.cpu_times_percent(interval=1))
-    time.sleep(temps)
-    count += 1
-
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     filename="log.log",
                     filemode="a",
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
+while count < max_temps:
+    print(datetime.datetime.now().time(),
+          logging.info(psutil.cpu_times_percent(interval=1)))
+    time.sleep(temps)
+    count += 1
 
-logging.info(psutil.cpu_times_percent(interval=1))
+print(psutil.virtual_memory())
+print(psutil.cpu_stats())
