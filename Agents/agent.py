6fa105ff8713fc0bfd0e69720bb7c4a583f709dc
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2023-05-11 11:21:46 +0200

    resolving the error causing the case not working

--- Agents/agent.py
+++ Agents/agent.py
@@ -15,35 +15,31 @@ try:
     # Loop infinite to print every 15 our info
     while True:
       # match:case condition to log everything in different interval depending the count
-        match COUNT:
-            case 1:
-                print(datetime.datetime.now().time(),
-                      psutil.cpu_times_percent(interval=0, percpu=False),
-                      # logging.info(psutil.cpu_percent(interval=None, percpu=False)),
-                      )
-                logging.info(psutil.cpu_times_percent(
-                    interval=0, percpu=False))
+      if COUNT == 1 :
+            print(datetime.datetime.now().time(),
+                  psutil.cpu_times_percent(interval=0, percpu=False),
+                  # logging.info(psutil.cpu_percent(interval=None, percpu=False)),
+                  )
+            logging.info(psutil.cpu_times_percent(interval=0, percpu=False))
                 # logging.info(psutil.cpu_percent(interval=None, percpu=False))
-                COUNT += 1
-                time.sleep(10)
-            case 2:
-                print(datetime.datetime.now().time(),
-                      # psutil.virtual_memory(),
-                      (psutil.virtual_memory().total - psutil.virtual_memory().available) / \
-                      psutil.virtual_memory().total * 100
-                      )
-                # logging.info(psutil.virtual_memory())
-                logging.info((psutil.virtual_memory(
-                ).total - psutil.virtual_memory().available) / psutil.virtual_memory().total * 100)
-                COUNT += 1
-                time.sleep(10)
-            case 3:
-                print(datetime.datetime.now().time(),
-                      psutil.disk_io_counters(perdisk=False, nowrap=True))
-                logging.info(psutil.disk_io_counters(
-                    perdisk=False, nowrap=True))
-                COUNT = 1
-                time.sleep(10)
+            COUNT += 1
+            time.sleep(10)
+      elif COUNT == 2 :
+            print(datetime.datetime.now().time(),
+                  # psutil.virtual_memory(),
+                  (psutil.virtual_memory().total - psutil.virtual_memory().available) / psutil.virtual_memory().total * 100)
+            # logging.info(psutil.virtual_memory())
+            logging.info((psutil.virtual_memory(
+            ).total - psutil.virtual_memory().available) / psutil.virtual_memory().total * 100)
+            COUNT += 1
+            time.sleep(10)
+      elif COUNT == 3 :
+            print(datetime.datetime.now().time(),
+                  psutil.disk_io_counters(perdisk=False, nowrap=True))
+            logging.info(psutil.disk_io_counters(
+                  perdisk=False, nowrap=True))
+            COUNT = 1
+            time.sleep(10)
 
 
 except Exception as e:
